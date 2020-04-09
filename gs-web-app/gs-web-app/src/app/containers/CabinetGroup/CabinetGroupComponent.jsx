4dea95360f7eeea856d9083a6b75ed7ca4b02e9c
west <west@syneforge.com>
2020-04-09 23:10:51 +0300

    added cabinet page without styles

--- gs-web-app/gs-web-app/src/app/containers/CabinetGroup/CabinetGroupComponent.jsx
+++ gs-web-app/gs-web-app/src/app/containers/CabinetGroup/CabinetGroupComponent.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
