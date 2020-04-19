5ade111748b8888368d6abff8a9be72c28b79108
west <west@syneforge.com>
2020-04-19 16:10:25 +0300

    changing cabinet

--- gs-web-app/gs-web-app/src/app/components/Menu/Menu.jsx
+++ gs-web-app/gs-web-app/src/app/components/Menu/Menu.jsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import {makeSelectUserData} from "../../state/selectors/user";
 import {connect} from "react-redux";
 import MenuComponent from "./MenuComponent";
+import {createStructuredSelector} from "reselect";
 
-const mapStateToProps = () => {
+const mapStateToProps = createStructuredSelector({
   userData: makeSelectUserData()
-};
+});
 
 const Menu = (props) => {
-  console.log(props, 'props hello');
 
   return (
     <MenuComponent />
