3ee90427fccc4756b8e84c0692b1299192d32171
west <west@syneforge.com>
2020-06-01 22:09:19 +0300

    add validation for step registration

--- gs-web-app/gs-web-app/src/app/containers/Registration/RegistrationFormCompoent.jsx
+++ gs-web-app/gs-web-app/src/app/containers/Registration/RegistrationFormCompoent.jsx
@@ -5,8 +5,11 @@ import Typography from '@material-ui/core/Typography';
 import {CardContent} from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import RegistrationForm from "./components/RegistrationForm/index";
-import {reduxForm } from "redux-form";
+import {reduxForm, formValueSelector } from "redux-form";
 import userRegistrationService from '../../services/userRegistrationService';
+import {connect} from "react-redux";
+import State from '../../state/ducks/index';
+import {compose} from "recompose";
 
 const useStyles = makeStyles(theme => ({
     Title: {
@@ -18,7 +21,39 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-const FormRegistration = reduxForm({form: 'registration'})(RegistrationForm);
+const selector = formValueSelector('registration');
+
+const mapStateToProps = (state: State) => {
+   return {
+       name: selector(state, 'name'),
+       surname: selector(state, 'surname'),
+       patronymic: selector(state, 'patronymic'),
+       sex: selector(state, 'sex'),
+       birthDate: selector(state, 'birth_date'),
+
+       specialty: selector(state, 'specialty'),
+       faculty: selector(state, 'faculty'),
+       department: selector(state, 'department'),
+
+       graduationUniversity: selector(state, 'graduation_university'),
+       graduationYear: selector(state, 'graduation_year'),
+       isHonorsDegree: selector(state, 'isHonorsDegree'),
+       email: selector(state, 'email'),
+       contactPhone: selector(state, 'contact_phone'),
+       publicationsCount: selector(state, 'publications_count'),
+       prospectiveSupervisor: selector(state, 'prospective_supervisor'),
+       distinctiveAwards: selector(state, 'distinctive_awards'),
+       additionalInfo: selector(state, 'additional_info'),
+
+   }
+};
+
+const hoc = compose(
+  connect(mapStateToProps),
+  reduxForm({form: 'registration'}),
+);
+
+const FormRegistration = hoc(RegistrationForm);
 
 const onSubmit = (formData) => {
     userRegistrationService.registerStudent(formData);
